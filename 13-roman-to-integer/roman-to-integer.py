f26d24d6686a3a194826d11b10e3cf7087b784fd
sasudo2 <71052914+sasudo2@users.noreply.github.com>
2026-05-15 22:31:28 +0545

    Time: 9 ms (19.48%) | Memory: 19.2 MB (58.37%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,57 +1,16 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        l = list(s)
+        length = len(s)
+        dict1 = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M':1000}
+        dict2 = {'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
         number = 0
-        while l:
-            found = False
-            if len(l) >= 2:
-                if l[0] == "C"  and l[1] == "M":
-                    l.pop(0)
-                    l.pop(0)
-                    number += 900
-                    found = True
-                elif l[0] == "C" and l[1] == "D":
-                    l.pop(0)
-                    l.pop(0)
-                    number += 400
-                    found = True
-                elif l[0] == "X" and l[1] == "C":
-                    l.pop(0)
-                    l.pop(0)
-                    number += 90
-                    found = True
-                elif l[0] == "X" and l[1] == "L":
-                    l.pop(0)
-                    l.pop(0)
-                    number += 40
-                    found = True
-                elif l[0] == 'I' and l[1] == 'X':
-                    l.pop(0)
-                    l.pop(0)
-                    number += 9
-                    found = True
-                elif l[0] == 'I' and l[1] == 'V':
-                    l.pop(0)
-                    l.pop(0)
-                    number += 4
-                    found = True
-                else:
-                    pass
-                
-            if found == False:
-                num = l.pop(0)
-                if num == "M":
-                    number += 1000
-                elif num == "D":
-                    number += 500
-                elif num == "C":
-                    number += 100
-                elif num == 'L':
-                    number += 50
-                elif num == "X":
-                    number += 10
-                elif num == "V":
-                    number += 5
-                elif num == "I":
-                    number += 1
+        x = 0
+        while x < length:
+            if s[x:x+2] in dict2:
+                number += dict2[s[x:x+2]]
+                x += 2
+            else:
+                number += dict1[s[x]]
+                x += 1
+
         return number
